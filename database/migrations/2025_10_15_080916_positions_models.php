f5d2dd68d677351cd158f4eb83cec30eb6c49743
vsev92dev <vsev92@mail.ru>
2025-10-15 15:53:02 +0700

    create migrations

---
+++ database/migrations/2025_10_15_080916_positions_models.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('positions_models', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('position_id')->constrained('staff_positions')->onDelete('cascade');
+            $table->foreignId('model_id')->constrained('car_models')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('positions_models');
+    }
+};
